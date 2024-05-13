2681f2ef3cc20a3bfa7f7dfa0fd4597fd5b8204e
Shahmeer-09 <shahmeerweb@gmail.com>
2024-05-13 16:33:42 +0500

    .

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ const { errorManager } = require("./middlewares/errormanager");
 const app = express();
 const path = require("path");
 
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(express.json());
+app.use(express.json({limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.use("/thread/user", require("./Routes/user.route"));
 app.use("/thread/post", require("./Routes/post.route"));
 app.get("*", (req, res) => {
@@ -32,4 +31,3 @@ connectDb()
     });
   })
   .catch((err) => console.log(err));
-  module.exports = app;
