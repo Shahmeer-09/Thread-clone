ba1be69514d110ed33c3fae0481ce5e627a0973b
Shahmeer-09 <shahmeerweb@gmail.com>
2024-05-13 14:59:27 +0500

    deployment setup adjustment

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
-require('dotenv').config()
-const cookieParser = require('cookie-parser')
-const {connectDb} = require('./config/connectDb')
-const express = require('express')
-const bodyParser = require('body-parser')
-const { StatusCodes } = require('http-status-codes')
-const {errorManager} = require('./middlewares/errormanager')
-const app = express()
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const { connectDb } = require("./config/connectDb");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { StatusCodes } = require("http-status-codes");
+const { errorManager } = require("./middlewares/errormanager");
+const app = express();
+const path = require("path");
 
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
-
-app.use('/thread/user', require('./Routes/user.route'))
-app.use('/thread/post', require('./Routes/post.route'))
-
-app.get('*', (req,res)=>{
-    res.status(StatusCodes.NOT_FOUND).json("route not found")
+app.use("/thread/user", require("./Routes/user.route"));
+app.use("/thread/post", require("./Routes/post.route"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 })
-app.use(errorManager)
+app.get("*", (req, res) => {
+  res.status(StatusCodes.NOT_FOUND).json("route not found");
+});
+app.use(errorManager);
 
-
-const port  = process.env.PORT || 5100
-connectDb().then(()=>{
-    console.log('database connected successfully')
-    app.listen(port, ()=>{
-        console.log(`server is running on port ${port}`)
-    })
-}).catch(err=>console.log(err)) 
+const port = process.env.PORT || 5100;
+connectDb()
+  .then(() => {
+    console.log("database connected successfully");
+    app.listen(port, () => {
+      console.log(`server is running on port ${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
